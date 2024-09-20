77063fdb2d5a895debce64a7673832fba0147e06
Dev_Nergis <136839304+DevNergis@users.noreply.github.com>
2024-09-20 21:01:28 +0900

    Add: first commit
    Use Astro
    
    Add 학사 일정

---
+++ astro.config.mjs
@@ -0,0 +1,13 @@
+// @ts-check
+import { defineConfig } from 'astro/config';
+
+import icon from 'astro-icon';
+
+import react from '@astrojs/react';
+
+import tailwind from '@astrojs/tailwind';
+
+// https://astro.build/config
+export default defineConfig({
+  integrations: [icon(), react(), tailwind()]
+});
