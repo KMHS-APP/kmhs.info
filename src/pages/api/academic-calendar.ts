66c689d3f23f4e6e77003a070c815135ac669fda
Dev_Nergis <136839304+DevNergis@users.noreply.github.com>
2024-10-10 01:44:11 +0900

    제대로 작동하는 SSR

---
+++ src/pages/api/academic-calendar.ts
@@ -0,0 +1,13 @@
+import type { APIRoute } from 'astro';
+
+export const prerender = true;
+
+// @ts-ignore
+export const GET: APIRoute = async () => {
+    const currentYear = new Date().getFullYear();
+    const fromDate = `${currentYear}0301`;
+    const toDate = `${currentYear}1231`;
+    const response = await fetch(`https://open.neis.go.kr/hub/SchoolSchedule?KEY=4e1af35dc1954eb8aaf573eff8653c43&Type=json&ATPT_OFCDC_SC_CODE=J10&SD_SCHUL_CODE=7531381&AA_FROM_YMD=${fromDate}&AA_TO_YMD=${toDate}&pSize=365`);
+    const data = await response.json();
+    return new Response(JSON.stringify(data.SchoolSchedule[1].row), { status: 200 });
+};
---
+++ src/pages/api/feeding.ts
@@ -0,0 +1,13 @@
+import type { APIRoute } from 'astro';
+
+export const prerender = true;
+
+// @ts-ignore
+export const GET: APIRoute = async () => {
+    const currentYear = new Date().getFullYear();
+    const fromDate = `${currentYear}0101`;
+    const toDate = `${currentYear}1231`;
+    const response = await fetch(`https://open.neis.go.kr/hub/mealServiceDietInfo?KEY=4e1af35dc1954eb8aaf573eff8653c43&Type=json&ATPT_OFCDC_SC_CODE=J10&SD_SCHUL_CODE=7531381&MLSV_FROM_YMD=${fromDate}&MLSV_TO_YMD=${toDate}&pSize=365`);
+    const data = await response.json();
+    return new Response(JSON.stringify(data.mealServiceDietInfo[1].row), { status: 200 });
+};
