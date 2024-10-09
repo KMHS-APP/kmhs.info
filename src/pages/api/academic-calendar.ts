b647c62340cc4450713a4f6368ebdaef063917a8
Dev_Nergis <136839304+DevNergis@users.noreply.github.com>
2024-10-10 02:34:37 +0900

    fuck astro bug

--- src/pages/api/academic-calendar.ts
+++ src/pages/api/academic-calendar.ts
@@ -9,5 +9,5 @@ export const GET: APIRoute = async () => {
     const toDate = `${currentYear}1231`;
     const response = await fetch(`https://open.neis.go.kr/hub/SchoolSchedule?KEY=4e1af35dc1954eb8aaf573eff8653c43&Type=json&ATPT_OFCDC_SC_CODE=J10&SD_SCHUL_CODE=7531381&AA_FROM_YMD=${fromDate}&AA_TO_YMD=${toDate}&pSize=365`);
     const data = await response.json();
-    return new Response(JSON.stringify(data.SchoolSchedule[1].row), { status: 200 });
+    return new Response(JSON.stringify(data.SchoolSchedule[1].row), { status: 200, headers: { 'Content-Type': 'application/json' } });
 };
