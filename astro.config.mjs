b0dcb161e77ad62ee51393d2b93e83cfc843f337
Dev_Nergis <136839304+DevNergis@users.noreply.github.com>
2024-10-10 02:44:37 +0000

    f

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,10 +8,16 @@ import react from '@astrojs/react';
 import tailwind from '@astrojs/tailwind';
 
 import cloudflare from '@astrojs/cloudflare';
+import { parseIsolatedEntityName } from 'typescript';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [icon(), react(), tailwind()],
+  integrations: [icon({
+    include: {
+      mdi: ['mdi-home'],
+      fluent-emoji: ['fork-and-knife-with-plate']
+    }
+  }), react(), tailwind()],
   output: 'server',
   adapter: cloudflare({
     imageService: 'cloudflare'
