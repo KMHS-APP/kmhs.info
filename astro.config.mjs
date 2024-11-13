cd2fe6882c63568bad09c2568bd4563b5062c5f3
Dev_Nergis <136839304+DevNergis@users.noreply.github.com>
2024-11-13 03:35:54 +0000

    링크 카드 컴포넌트 추가 및 인스타그램, YouTube 아이콘 지원: 새로운 URL 페이지 및 JSON 데이터 추가, 아이콘 목록 업데이트

--- astro.config.mjs
+++ astro.config.mjs
@@ -13,7 +13,7 @@ import cloudflare from '@astrojs/cloudflare';
 export default defineConfig({
   integrations: [icon({
     include: {
-      mdi: ['home', 'snowflake', 'about-circle-outline', 'chevron-up', 'link', 'ios-share', 'arrow-down-box'],
+      mdi: ['home', 'snowflake', 'about-circle-outline', 'chevron-up', 'link', 'ios-share', 'arrow-down-box', 'instagram', 'link-box-variant-outline', 'youtube'],
       'icon-park-twotone': ['degree-hat'],
       'fluent-emoji-high-contrast': ['fork-and-knife-with-plate']
     }
