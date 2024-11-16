0469d074de2fbe139b381870cb037132c36d5de4
Dev_Nergis <136839304+DevNergis@users.noreply.github.com>
2024-11-16 16:51:00 +0900

    wow

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,10 +1,1 @@
 /// <reference path="../.astro/types.d.ts" />
-
-interface ImportMetaEnv {
-    readonly SUPABASE_URL: string
-    readonly SUPABASE_ANON_KEY: string
-}
-
-interface ImportMeta {
-    readonly env: ImportMetaEnv
-}
