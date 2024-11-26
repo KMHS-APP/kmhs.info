bc7466ee3478e590eaa0ef3e27281707d5a848fa
Dev_Nergis <136839304+DevNergis@users.noreply.github.com>
2024-11-26 01:48:13 +0000

    도구 모음 페이지 추가 및 아이콘 업데이트, 패키지 버전 변경

--- astro.config.mjs
+++ astro.config.mjs
@@ -13,7 +13,7 @@ import cloudflare from '@astrojs/cloudflare';
 export default defineConfig({
   integrations: [icon({
     include: {
-      mdi: ['home', 'snowflake', 'about-circle-outline', 'chevron-up', 'link', 'ios-share', 'arrow-down-box', 'instagram', 'link-box-variant-outline', 'youtube', 'cloud-upload-outline', 'menu'],
+      mdi: ['home', 'snowflake', 'about-circle-outline', 'chevron-up', 'link', 'ios-share', 'arrow-down-box', 'instagram', 'link-box-variant-outline', 'youtube', 'cloud-upload-outline', 'menu', 'tools', 'toolbox-outline', 'timetable'],
       'icon-park-twotone': ['degree-hat'],
       'fluent-emoji-high-contrast': ['fork-and-knife-with-plate']
     }
