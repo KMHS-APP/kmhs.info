5c336068db75b5f0fa2b0b93aa9b56d7794a6939
Dev_Nergis <136839304+DevNergis@users.noreply.github.com>
2025-01-08 01:55:01 +0000

    Update academic calendar API to adjust date range based on current month

--- src/pages/api/academic-calendar.ts
+++ src/pages/api/academic-calendar.ts
@@ -2,9 +2,11 @@ import type { APIRoute } from "astro";
 
 // @ts-ignore
 export const GET: APIRoute = async () => {
-  const currentYear = new Date().getFullYear();
-  const fromDate = `${currentYear}0301`;
-  const toDate = `${currentYear}1231`;
+  const today = new Date();
+  const currentYear = today.getFullYear();
+  const year = today.getMonth() < 2 ? currentYear - 1 : currentYear; // 3월 1일 이전이면 전년도 사용
+  const fromDate = `${year}0301`;
+  const toDate = `${year}1231`;
   const response = await fetch(
     `https://open.neis.go.kr/hub/SchoolSchedule?KEY=4e1af35dc1954eb8aaf573eff8653c43&Type=json&ATPT_OFCDC_SC_CODE=J10&SD_SCHUL_CODE=7531381&AA_FROM_YMD=${fromDate}&AA_TO_YMD=${toDate}&pSize=365`,
   );
